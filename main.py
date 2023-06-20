69a0b53b7d2444e743db1fd8ae248de715755928
Riku <cmj14099@ict.nitech.ac.jp>
2023-06-21 02:31:41 +0900

    commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 from skimage.io import imread
 
+#Editted by Rickey
 
 # download the image
 img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
